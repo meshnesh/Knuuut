a64833ac9f53d44a0f65fc173bbf6a34431cce8d
meshnesh <tonny.nesh@gmail.com>
2016-06-08 20:38:59 +0300

    simulated a callback and request function

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('mango');
+function placeAnOrder (orderNumber ) {
+
+    console.log('Customer order:', orderNumber);
+
+
+    cookAndDeliverFood( function () {
+
+        console.log('food delivered order:', orderNumber);
+
+    });
+    
+}
+
+
+
+//simulate 5 second operation
+function cookAndDeliverFood (callback ) {
+
+    setTimeout(callback, 5000);
+    
+}
+
+// simulate customer order
+placeAnOrder(1);
+placeAnOrder(2);
+placeAnOrder(3);
+placeAnOrder(4);
+placeAnOrder(5);
