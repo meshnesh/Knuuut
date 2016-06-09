36cf6993af1729ca70b8855fe16baa7590c40eb3
meshnesh <tonny.nesh@gmail.com>
2016-06-09 19:04:15 +0300

    modified app.js

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
-function placeAnOrder (orderNumber ) {
+// function placeAnOrder (orderNumber ) {
+//
+//     console.log('Customer order:', orderNumber);
+//
+//
+//     cookAndDeliverFood( function () {
+//
+//         console.log('food delivered order:', orderNumber);
+//
+//     });
+//
+// }
+//
+//
+//
+// //simulate 5 second operation
+// function cookAndDeliverFood (callback ) {
+//
+//     setTimeout(callback, 5000);
+//
+// }
+//
+// // simulate customer order
+// placeAnOrder(1);
+// placeAnOrder(2);
+// placeAnOrder(3);
+// placeAnOrder(4);
+// placeAnOrder(5);
 
-    console.log('Customer order:', orderNumber);
 
-
-    cookAndDeliverFood( function () {
-
-        console.log('food delivered order:', orderNumber);
-
-    });
+function User() {
+    this.name = "";
+    this.life = 100;
+    this.giveLife = function giveLife(targetPlayer) {
+        targetPlayer.life += 1;
+        console.log(this.name + " gave 1 life to " + targetPlayer.name);
+        
+    }
     
 }
 
 
+var Dismas = new User();
+var Kiilu = new User();
+Dismas.name = "Dismas"
+Kiilu.name = "kiilu"
 
-//simulate 5 second operation
-function cookAndDeliverFood (callback ) {
 
-    setTimeout(callback, 5000);
-    
-}
+Dismas.giveLife(Kiilu);
+console.log("Dismas: " + Dismas.life);
+console.log("Kiilu: " + Kiilu.life);
 
-// simulate customer order
-placeAnOrder(1);
-placeAnOrder(2);
-placeAnOrder(3);
-placeAnOrder(4);
-placeAnOrder(5);
