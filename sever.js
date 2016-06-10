f9c86a4616e68dd8b5f8e45438df3a2d12d1667f
meshnesh <tonny.nesh@gmail.com>
2016-06-10 18:32:10 +0300

    added package.json file

--- sever.js
+++ sever.js
@@ -1,12 +1,25 @@
 var http = require('http');
+var fs = require('fs');
 
-
-function onRequest(request, response) {
-    console.log("A User made a request" + request.uri);
-    response.writeHead(200, {"Context-Type" : "text/plain"});
-    response.write("Here is some data");
+//404 response
+function send404Response(response) {
+    response.writeHead(404, {"Context-Type" : "text/plain"});
+    response.write("Error 404: Page not found!");
     response.end();
 }
 
+
+function onRequest(request, response) {
+
+    if( request.method == 'GET' && request.url == '/'){
+        response.writeHead(200, {"Context-Type" : "text/html"});
+        fs.createReadStream("./index").pipe(response);
+        response.end();
+    }else{
+        send404Response(response);
+    }
+
+}
+
 http.createServer(onRequest).listen(8888);
-console.log("Server is running now....."); 
+console.log("Server is running now.....");
