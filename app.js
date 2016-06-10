87bb3107145ca30628a2449110c19609c636571b
meshnesh <tonny.nesh@gmail.com>
2016-06-10 13:11:59 +0300

    imported and exported code

--- app.js
+++ app.js
@@ -1,64 +1,72 @@
-// function placeAnOrder (orderNumber ) {
+// // function placeAnOrder (orderNumber ) {
+// //
+// //     console.log('Customer order:', orderNumber);
+// //
+// //
+// //     cookAndDeliverFood( function () {
+// //
+// //         console.log('food delivered order:', orderNumber);
+// //
+// //     });
+// //
+// // }
+// //
+// //
+// //
+// // //simulate 5 second operation
+// // function cookAndDeliverFood (callback ) {
+// //
+// //     setTimeout(callback, 5000);
+// //
+// // }
+// //
+// // // simulate customer order
+// // placeAnOrder(1);
+// // placeAnOrder(2);
+// // placeAnOrder(3);
+// // placeAnOrder(4);
+// // placeAnOrder(5);
 //
-//     console.log('Customer order:', orderNumber);
 //
+// function User() {
+//     this.name = "";
+//     this.life = 100;
+//     this.giveLife = function giveLife(targetPlayer) {
+//         targetPlayer.life += 1;
+//         console.log(this.name + " gave 1 life to " + targetPlayer.name);
 //
-//     cookAndDeliverFood( function () {
-//
-//         console.log('food delivered order:', orderNumber);
-//
-//     });
+//     }
 //
 // }
 //
 //
+// var Dismas = new User();
+// var Kiilu = new User();
+// Dismas.name = "Dismas"
+// Kiilu.name = "kiilu"
 //
-// //simulate 5 second operation
-// function cookAndDeliverFood (callback ) {
 //
-//     setTimeout(callback, 5000);
+// Dismas.giveLife(Kiilu);
+// console.log("Dismas: " + Dismas.life);
+// console.log("Kiilu: " + Kiilu.life);
 //
-// }
 //
-// // simulate customer order
-// placeAnOrder(1);
-// placeAnOrder(2);
-// placeAnOrder(3);
-// placeAnOrder(4);
-// placeAnOrder(5);
-
-
-function User() {
-    this.name = "";
-    this.life = 100;
-    this.giveLife = function giveLife(targetPlayer) {
-        targetPlayer.life += 1;
-        console.log(this.name + " gave 1 life to " + targetPlayer.name);
-        
-    }
-    
-}
-
-
-var Dismas = new User();
-var Kiilu = new User();
-Dismas.name = "Dismas"
-Kiilu.name = "kiilu"
-
-
-Dismas.giveLife(Kiilu);
-console.log("Dismas: " + Dismas.life);
-console.log("Kiilu: " + Kiilu.life);
+//
+// User.prototype.uppercut = function uppercut(targetPlayer) {
+//     targetPlayer.life -= 3;
+//     console.log(this.name + " just uppercutted " + targetPlayer.name);
+//
+// };
+//
+//
+// Kiilu.uppercut(Dismas);
+// console.log("Dismas: " + Dismas.life);
+// console.log("Kiilu: " + Kiilu.life);
 
 
 
-User.prototype.uppercut = function uppercut(targetPlayer) {
-    targetPlayer.life -= 3;
-    console.log(this.name + " just uppercutted " + targetPlayer.name);
-    
-};
 
 
-Kiilu.uppercut(Dismas);
-console.log("Dismas: " + Dismas.life);
-console.log("Kiilu: " + Kiilu.life);
+var movies = require('./movies');
+movies.avatar();
+movies.flash();
---
+++ movies.js
@@ -0,0 +1,12 @@
+function printAvatar() {
+    console.log("Avatar PG 13");
+    
+}
+
+function printFlash() {
+    console.log("The Flash PG 11");
+
+}
+
+module.exports.avatar = printAvatar;
+module.exports.flash = printFlash;
